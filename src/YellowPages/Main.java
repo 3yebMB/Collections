c67eb7ede83b6f95ee1f3625d2632938524afe09
3yebmb <3yebmb@gmail.com>
2019-01-08 23:38:11 +0500

    release

--- src/YellowPages/Main.java
+++ src/YellowPages/Main.java
@@ -1,4 +1,27 @@
 package YellowPages;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
 public class Main {
+    public static void main(String[] args) {
+        final String surname1 = "Гагарин Ю.А.";
+        final String surname2 = "Леонов А.А.";
+        final String surname3 = "Терешкова В.В.";
+        final String surname4 = "Титов Г.С.";
+
+        Record phones = new Record();
+        phones.add(surname1, 88002000000L);
+        phones.add(surname2, 88002000001L);
+        phones.add(surname3, 88002000002L);
+        phones.add(surname4, 88002000003L);
+        phones.add(surname1, 88002000004L);
+        phones.add(surname2, 88002000005L);
+
+        phones.get(surname1);
+        phones.get("Леонов А.А.");
+
+    }
 }
--- src/YellowPages/Record.java
+++ src/YellowPages/Record.java
@@ -1,4 +1,22 @@
 package YellowPages;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class Record {
+    private String name;
+    private long phone;
+    HashMap<Long, String> yp = new HashMap<>();
+
+    public void add(String n, long ph){
+        yp.put(ph, n);
+    }
+
+    public void get(String n){
+        for (HashMap.Entry<Long, String>entry : yp.entrySet()){
+            if (n.equalsIgnoreCase(entry.getValue()))
+                System.out.printf("Аббонент %s, т. %s\n", entry.getValue(), entry.getKey());
+        }
+    }
 }
